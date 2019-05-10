f68657ffeb784c26e3c85725ce9db094aa223de5
TLQ <1906595568@qq.com>
2019-05-10 12:32:23 +0800

    初始化

---
+++ springboot-hellowrod/src/main/java/com/tlq/springboothellowrod/SpringbootHellowrodApplication.java
@@ -0,0 +1,13 @@
+package com.tlq.springboothellowrod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellowrodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHellowrodApplication.class, args);
+    }
+
+}
